b0f0379ac169b786e21d5987f40cd93040d4c278
Dоплер Dоплер <skillex91@mail.ru>
2021-06-01 15:29:31 +0300

    Добавьте файлы проекта.

---
+++ 01_06_M/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01_06_M
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Count_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToInt32(Value1.Text);
+            double b = Convert.ToInt32(Value2.Text);
+
+            if (Actions.SelectedIndex == 0)
+            {
+                
+                double result = Summ.Sum(a, b);
+                Result.Text = result.ToString();
+            }
+            if (Actions.SelectedIndex == 1)
+            {
+
+                double result = Subtr.Sub(a, b);
+                Result.Text = result.ToString();
+            }
+            if (Actions.SelectedIndex == 2)
+            {
+                
+
+                double result = Multi.Mult(a, b);
+                Result.Text = result.ToString();
+            }
+            if (Actions.SelectedIndex == 3)
+            {
+                
+
+                double result = Dividing.Div(a, b);
+                Result.Text = result.ToString();
+            }
+            if (Actions.SelectedIndex == 4)
+            {
+               
+
+                double result = MathPow.Pow(a, b);
+                Result.Text = result.ToString();
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _01_06_M;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSumma()
+        {
+            //arrange
+            double x = 1;
+            double y = 2;
+
+            double res = 3;
+            //act
+            var ex = Summ.Sum(x, y);
+            //assert
+            Assert.AreEqual(ex, res);
+        }
+        [TestMethod]
+        public void TestSubtr()
+        {
+            //arrange
+            double x = 1;
+            double y = 2;
+
+            double res = -1;
+            //act
+            var ex = Subtr.Sub(x, y);
+            //assert
+            Assert.AreEqual(ex, res);
+        }
+        [TestMethod]
+        public void TestMulti()
+        {
+            //arrange
+            double x = 1;
+            double y = 2;
+
+            double res = 2;
+            //act
+            var ex = Multi.Mult(x, y);
+            //assert
+            Assert.AreEqual(ex, res);
+        }
+        [TestMethod]
+        public void TestDividing()
+        {
+            //arrange
+            double x = 4;
+            double y = 2;
+
+            double res = 2;
+            //act
+            var ex = Dividing.Div(x, y);
+            //assert
+            Assert.AreEqual(ex, res);
+        }
+        [TestMethod]
+        public void TestMathPow()
+        {
+            //arrange
+            double x = 2;
+            double y = 2;
+
+            double res = 4;
+            //act
+            var ex = MathPow.Pow(x, y);
+            //assert
+            Assert.AreEqual(ex, res);
+        }
+    }
+}
